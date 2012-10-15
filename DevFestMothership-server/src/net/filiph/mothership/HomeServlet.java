cd7bcb89a17f9b288b8bb485003c347b7e0f225f
David Vavra <me@destil.cz>
2012-10-15 22:42:52 +0200

    Optional notification, vibrate & update on server
    Also design of the backdoor greatly improved :)

--- DevFestMothership-server/src/net/filiph/mothership/HomeServlet.java
+++ DevFestMothership-server/src/net/filiph/mothership/HomeServlet.java
@@ -30,47 +30,53 @@ import javax.servlet.http.HttpServletResponse;
 @SuppressWarnings("serial")
 public class HomeServlet extends BaseServlet {
 
-  static final String ATTRIBUTE_STATUS = "status";
+	static final String ATTRIBUTE_STATUS = "status";
 
-  /**
-   * Displays the existing messages and offer the option to send a new one.
-   */
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws IOException {
-    resp.setContentType("text/html");
-    resp.setCharacterEncoding("utf-8");
-    PrintWriter out = resp.getWriter();
+	/**
+	 * Displays the existing messages and offer the option to send a new one.
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding("utf-8");
+		PrintWriter out = resp.getWriter();
 
-    out.print("<html><body>");
-    out.print("<head>");
-    out.print("  <title>Mothership Backdoor</title>");
-    out.print("  <link rel='icon' href='/favicon.png'/>");
-    out.print("  <meta charset='utf-8' />");
-    out.print("</head>");
-    String status = (String) req.getAttribute(ATTRIBUTE_STATUS);
-    if (status != null) {
-      out.print(status);
-    }
-    int total = Datastore.getTotalDevices();
-    if (total == 0) {
-      out.print("<h2>No devices registered!</h2>");
-    } else {
-      out.print("<h3>" + total + " device(s) registered!</h3>");
-      out.print("<h2>Override AI:</h2>");
-      out.print("<form name='form' method='POST' action='sendAll'>");
-      out.print("<textarea name='message' cols='40' rows='4'></textarea><br />");
-      out.print("<input type='submit' value='Send to all humans' />");
-      out.print("</form>");
-    }
-    out.print("</body></html>");
-    resp.setStatus(HttpServletResponse.SC_OK);
-  }
+		out.print("<html><body>");
+		out.print("<head>");
+		out.print("  <title>Agnes Backdoor</title>");
+		out.print("  <link rel='icon' href='/favicon.ico'/>");
+		out.print("  <meta charset='utf-8' />");
+		out.print("</head>");
+		out.print("<body style=\"background-image:url('matrix.gif'); color:white;text-align:center\"><div style=\"display:inline-block;background-color: rgba(0%, 0%, 0%, 0.7)\">");
+		String status = (String) req.getAttribute(ATTRIBUTE_STATUS);
+		if (status != null) {
+			out.print(status);
+		}
+		int total = Datastore.getTotalDevices();
+		if (total == 0) {
+			out.print("<h2>No slaves registered!</h2>");
+		} else {
+			out.print("<h3>" + total + " slave devices active!</h3>");
+			out.print("<h2>Override AI behavior:</h2>");
+			out.print("<form name='form' method='POST' action='sendAll'>");
+			out.print("<textarea name='message' cols='40' rows='4'></textarea><br />");
+			out.print("<input type='checkbox' name='notify' value='true' /> Flood their notifications<br />");
+			out.print("<input type='checkbox' name='vibrate' value='true' /> Vibrate-shock their pockets<br />");
+			out.print("<input type='submit' value='Push to all humans' />");
+			out.print("</form>");
+			out.print("<h2>Update hard-wired personality files:</h2>");
+			out.print("<form name='form' method='POST' action='sendAll'>");
+			out.print("<input type='hidden' name='update' value='true' />");
+			out.print("<input type=\"submit\" value=\"Let's hope she won't notice\" />");
+			out.print("</form>");
+		}
+		out.print("</div></body></html>");
+		resp.setStatus(HttpServletResponse.SC_OK);
+	}
 
-  @Override
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-      throws IOException {
-    doGet(req, resp);
-  }
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		doGet(req, resp);
+	}
 
 }
