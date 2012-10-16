294876b1fb9dd72450f8fa6616be30cc99b122c1
Filip Hracek <filiph@google.com>
2012-10-16 15:18:03 +0200

    Added suport for [nv] metaoptions in GCM messages.

--- src/net/filiph/mothership/gcm/CommonUtilities.java
+++ src/net/filiph/mothership/gcm/CommonUtilities.java
@@ -21,6 +21,8 @@ import net.filiph.mothership.MainActivity;
 import net.filiph.mothership.NotificationHelper;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
+import android.util.Log;
 
 /**
  * Helper class providing methods and constants common to other classes in the
@@ -55,28 +57,78 @@ public final class CommonUtilities {
     public static final String EXTRA_MESSAGE = "message";
 
     /**
-     * Notifies UI to display a message.
+     * Notifies UI to display a message. 
+     * <p>
+     * Vibrates and/or notifies according to
+     * the content of the messageString: "[nv] Hello" will both vibrate and notify.
+     * "[n] Hello" will just notify. "[] Hello" will only update message, but will
+     * not notify user in any way (unless they are looking at the app at the moment).
+     * Unimplemented "[u]" will make the application get new message schedule from web.
      * <p>
      * This method is defined in the common helper because it's used both by
      * the UI and the background service.
      *
      * @param context application's context.
-     * @param message message to be displayed.
+     * @param message message to be displayed. Should start with "[]" meta options.
      */
     public static void displayMessage(Context context, String messageString) {
-		// put the newest message into a pref so we show it even after resuming the app
-		context.getSharedPreferences(MainActivity.PREFS_NAME, 0).edit()
-			.putString("gcmMessageString", messageString)
-			.putLong("gcmMessageTime", new Date().getTime())
-			.commit();
+    	// defaults
+    	boolean vibrate = false;
+    	boolean notify = false;
+    	boolean update = false;
     	
-//        Intent intent = new Intent(DISPLAY_MESSAGE_ACTION);
-//        intent.putExtra(EXTRA_MESSAGE, messageString);
-//        context.sendBroadcast(intent);
-		
-		Intent updateIntent = new Intent("NewMothershipMessage");
-		context.sendBroadcast(updateIntent);
-        
-        NotificationHelper.notify(context, true); // notify on new message and vibrate (TODO: let GCM sender choose)
+    	// extract meta information
+    	int startBracketIndex = messageString.indexOf("[");
+    	int endBracketIndex = messageString.indexOf("]");
+    	if (startBracketIndex == 0 && endBracketIndex > 0) {
+    		for (int i = 1; i < endBracketIndex; i++) {
+    			char ch = messageString.charAt(i);
+    			switch (ch) {
+    				case 'v': 
+    					vibrate = true;
+    					break;
+    				case 'n':
+    					notify = true;
+    					break;
+    				case 'u':
+    					update = true;
+    					break;
+    			}
+    		}
+    	} else {
+    		Log.w(TAG, "Received message without '[]' options in the front.");
+    	}
+    	
+    	if (update) {
+    		// TODO: update message schedule from web JSON
+    	}
+    	
+    	if (endBracketIndex < messageString.length() - 1) {
+    		String message = messageString.substring(endBracketIndex + 1);
+
+			// put the newest message into a pref so we show it even after resuming the app
+			context.getSharedPreferences(MainActivity.PREFS_NAME, 0).edit()
+				.putString("gcmMessageString", message)
+				.putLong("gcmMessageTime", new Date().getTime())
+				.commit();
+	    	
+	//        Intent intent = new Intent(DISPLAY_MESSAGE_ACTION);
+	//        intent.putExtra(EXTRA_MESSAGE, messageString);
+	//        context.sendBroadcast(intent);
+			
+			Intent updateIntent = new Intent("NewMothershipMessage");
+			context.sendBroadcast(updateIntent);
+    	}
+			
+		if (notify) {
+			NotificationHelper.notify(context, vibrate); // notify on new message and vibrate
+		} else if (vibrate) {
+			Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+			long[] pattern = {
+					0, 	// start immediately
+					300	// vibrate for 300ms 
+			};
+			v.vibrate(pattern, -1);
+		}
     }
 }
